fb7ed183a5a39c1230cdbd4657930732786f6fbc
Yary Ribero <yary.ribero@ignite.ee>
2018-03-27 16:17:55 +0300

    Fork configuration

--- conf.js
+++ conf.js
@@ -9,11 +9,11 @@ exports.bLight = false;
 exports.storage = 'sqlite';
 
 
-exports.hub = 'byteball.org/bb';
+exports.hub = 'test-hub.dagcoin.org/spoon/';
 exports.deviceName = 'Witness';
 exports.permanent_pairing_secret = 'randomstring';
-exports.admin_email = "admin@example.com",
-exports.from_email = "fromemail@example.com",
+exports.admin_email = "admin@example.com";
+exports.from_email = "fromemail@example.com";
 exports.control_addresses = ['DEVICE ALLOWED TO CHAT'];
 exports.payout_address = 'WHERE THE MONEY CAN BE SENT TO';
 
@@ -22,5 +22,13 @@ exports.THRESHOLD_DISTANCE = 50;
 exports.MIN_AVAILABLE_WITNESSINGS = 100;
 
 exports.KEYS_FILENAME = 'keys.json';
+
+//Whether to redirect the output stream to a log file
+exports.PIPE_OUTPUT_TO_FILE=false;
+//Whether to use a password from the configuration file instead of asking it from the user
+exports.INTERACT_WITH_USER=false;
+//Wallet password. Relevant only in case INTERACT_WITH_USER is false.
+exports.WALLET_PASSWORD='BSh5COP5ZesCgmZPyPmB'; //Mock password.
+
 
 console.log('finished witness conf');
