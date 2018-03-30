2b0575cc440c7023fb5a5db342ceec6f45c7731e
Eugene Ilagin <eugene.ilagin@gmail.com>
2018-03-30 15:47:18 +0300

    Add airbnb linter to witness code #958

--- check_daemon.js
+++ check_daemon.js
@@ -1,6 +1,8 @@
-/*jslint node: true */
-"use strict";
-var check_daemon = require('core/check_daemon.js');
+/* jslint node: true */
+
+'use strict';
+
+const check_daemon = require('core/check_daemon.js');
 
 check_daemon.checkDaemonAndNotify('node start.js');
 
